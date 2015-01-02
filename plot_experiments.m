1b2ce903407c59451a0627b97b88c2d9dc3c7ac8
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-01-02 17:18:33 -0500

    added plots without the annealing expirements.

--- plot_experiments.m
+++ plot_experiments.m
@@ -82,3 +82,39 @@ for nd = 1:length(datasets)
   saveas(h, ['eps/', datasets{nd},'_mistakerate_no_annealing.eps'], 'eps2c')
   close all;
 end
+%% plot no annealing
+clc;
+clear;
+close all;
+
+load mat/mistakes_experiment_anneal.mat
+
+fs = 20;
+lw = 2;
+
+for nd = 1:length(datasets)
+  h = figure;
+  hold on;
+  box on;
+  
+  mistakes = mistakes_oba{nd};
+  plot(mean(cumsum(mistakes(:, 1:end-1)), 2), 'c', 'LineWidth', lw)
+  plot(cumsum(mistakes(:, end)), 'r', 'LineWidth', lw)
+
+  mistakes = mistakes_obo{nd};
+  plot(cumsum(mistakes(:, end)), 'k', 'LineWidth', lw)
+  
+  mistakes = mistakes_rba{nd};
+  plot(cumsum(mistakes(:, end)), 'b', 'LineWidth', lw)
+  
+  mistakes = mistakes_rbo{nd};
+  plot(cumsum(mistakes(:, end)), 'm', 'LineWidth', lw)
+  axis tight;
+  legend('Single', 'OFS-Bag', 'OFS-Boo', 'OFS-Bag-R', 'OFS-Boo-R', 'Location', 'Best')
+  set(gca, 'fontsize', fs)
+  xlabel('time', 'FontSize', fs)
+  ylabel('mistakes', 'FontSize', fs)
+  
+  saveas(h, ['eps/', datasets{nd},'_mistakes_annealing.eps'], 'eps2c')
+  close all;
+end
