d703263e0791ff9db796de13bfb03b71c3ccfc47
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-02-13 15:40:08 -0500

    made the plotting of the results a bit more flexible and easy to extend.

--- plot_experiments.m
+++ plot_experiments.m
@@ -3,7 +3,17 @@ clc;
 clear;
 close all;
 
-load mat/mistakes_experiment_no_anneal.mat
+% mistakes_experiment_anneal
+% mistakes_experiment_anneal_fixedPartial
+% mistakes_experiment_anneal_partialtest
+% mistakes_experiment_anneal_partialtest_fixedPartial
+% mistakes_experiment_no_anneal
+% mistakes_experiment_no_anneal_fixedPartial
+% mistakes_experiment_no_anneal_partialtest
+% mistakes_experiment_no_anneal_partialtest_fixedPartial
+fname = 'mistakes_experiment_no_anneal_partialtest_fixedPartial';
+
+load(['mat/', fname, '.mat']);
 
 fs = 20;
 lw = 2;
@@ -31,7 +41,7 @@ for nd = 1:length(datasets)
   xlabel('time', 'FontSize', fs)
   ylabel('mistakes', 'FontSize', fs)
   
-  saveas(h, ['eps/', datasets{nd},'_mistakes_no_annealing.eps'], 'eps2c')
+  saveas(h, ['eps/', datasets{nd}, '_', fname, '.eps'], 'eps2c')
   close all;
 end
 %% print times 
@@ -39,6 +49,14 @@ clc;
 clear;
 close all;
 
+% mistakes_experiment_anneal
+% mistakes_experiment_anneal_fixedPartial
+% mistakes_experiment_anneal_partialtest
+% mistakes_experiment_anneal_partialtest_fixedPartial
+% mistakes_experiment_no_anneal
+% mistakes_experiment_no_anneal_fixedPartial
+% mistakes_experiment_no_anneal_partialtest
+% mistakes_experiment_no_anneal_partialtest_fixedPartial
 load mat/mistakes_experiment_no_anneal.mat
 
 disp('Data Set & OFS-Bag & OFS-Boo & OFS-Bag-R & OFS-Boo-R')
@@ -51,6 +69,14 @@ clc;
 clear;
 close all;
 
+% mistakes_experiment_anneal
+% mistakes_experiment_anneal_fixedPartial
+% mistakes_experiment_anneal_partialtest
+% mistakes_experiment_anneal_partialtest_fixedPartial
+% mistakes_experiment_no_anneal
+% mistakes_experiment_no_anneal_fixedPartial
+% mistakes_experiment_no_anneal_partialtest
+% mistakes_experiment_no_anneal_partialtest_fixedPartial
 load mat/mistakes_experiment_no_anneal.mat
 
 disp('Data Set & Single & OFS-Bag & OFS-Boo & OFS-Bag-R & OFS-Boo-R')
@@ -77,76 +103,4 @@ for nd = 1:length(datasets)
   disp([datasets{nd}, ' & ', num2str(sum(mean(mistakes_oba{nd}(:,1:end-1),2))/(size(data,1)-1)), ...
     ' & ', num2str(sum(mistakes_oba{nd}(:,end))/(size(data,1)-1)), ' & ', num2str(sum(mistakes_obo{nd}(:,end))/(size(data,1)-1)), ...
     ' & ', num2str(sum(mistakes_rba{nd}(:,end))/(size(data,1)-1)), ' & ', num2str(sum(mistakes_rbo{nd}(:,end))/(size(data,1)-1))])
-end
-%% plot mistake RATE no annealing
-clc;
-clear;
-close all;
-
-load mat/mistakes_experiment_no_anneal.mat
-
-fs = 20;
-lw = 2;
-
-for nd = 1:length(datasets)
-  h = figure;
-  hold on;
-  box on;
-  
-  mistakes = mistakes_oba{nd};
-  plot(mean(cumsum(mistakes(:, 1:end-1)), 2)'./(1:length(mistakes(:, end))), 'c', 'LineWidth', lw)
-  plot(cumsum(mistakes(:, end))'./(1:length(mistakes(:, end))), 'r', 'LineWidth', lw)
-
-  mistakes = mistakes_obo{nd};
-  plot(cumsum(mistakes(:, end))'./(1:length(mistakes(:, end))), 'k', 'LineWidth', lw)
-  
-  mistakes = mistakes_rba{nd};
-  plot(cumsum(mistakes(:, end))'./(1:length(mistakes(:, end))), 'b', 'LineWidth', lw)
-  
-  mistakes = mistakes_rbo{nd};
-  plot(cumsum(mistakes(:, end))'./(1:length(mistakes(:, end))), 'm', 'LineWidth', lw)
-  axis tight;
-  legend('Single', 'OFS-Bag', 'OFS-Boo', 'OFS-Bag-R', 'OFS-Boo-R', 'Location', 'Best')
-  set(gca, 'fontsize', fs)
-  xlabel('time', 'FontSize', fs)
-  ylabel('mistakes', 'FontSize', fs)
-  
-  saveas(h, ['eps/', datasets{nd},'_mistakerate_no_annealing.eps'], 'eps2c')
-  close all;
-end
-%% plot no annealing
-clc;
-clear;
-close all;
-
-load mat/mistakes_experiment_anneal.mat
-
-fs = 20;
-lw = 2;
-
-for nd = 1:length(datasets)
-  h = figure;
-  hold on;
-  box on;
-  
-  mistakes = mistakes_oba{nd};
-  plot(mean(cumsum(mistakes(:, 1:end-1)), 2), 'c', 'LineWidth', lw)
-  plot(cumsum(mistakes(:, end)), 'r', 'LineWidth', lw)
-
-  mistakes = mistakes_obo{nd};
-  plot(cumsum(mistakes(:, end)), 'k', 'LineWidth', lw)
-  
-  mistakes = mistakes_rba{nd};
-  plot(cumsum(mistakes(:, end)), 'b', 'LineWidth', lw)
-  
-  mistakes = mistakes_rbo{nd};
-  plot(cumsum(mistakes(:, end)), 'm', 'LineWidth', lw)
-  axis tight;
-  legend('Single', 'OFS-Bag', 'OFS-Boo', 'OFS-Bag-R', 'OFS-Boo-R', 'Location', 'Best')
-  set(gca, 'fontsize', fs)
-  xlabel('time', 'FontSize', fs)
-  ylabel('mistakes', 'FontSize', fs)
-  
-  saveas(h, ['eps/', datasets{nd},'_mistakes_annealing.eps'], 'eps2c')
-  close all;
 end
